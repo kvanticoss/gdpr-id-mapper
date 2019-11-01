3200ac59d264b4798b1c5416100152ecc30f5c07
Mikael Rapp <micke.rapp@gmail.com>
2019-11-01 11:00:10 +0100

    Update build to match cloud build limitations

--- scripts/get_version.sh
+++ scripts/get_version.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# Will echo [{maybe git tag}-]{short commit hash}
-TAG=$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null)
-if [ $? -eq 0 ] ; then
-  TAG=$TAG-
-fi
-echo "$TAG$(git log -n1 --pretty='%h')"
+# Will echo [{maybe git tag}]_{short commit hash}
+echo "$(git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null)$TAG$(git log -n1 --pretty='%h')"
